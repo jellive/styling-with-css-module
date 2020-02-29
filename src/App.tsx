3909bdc2b399544b56965dd1c03b28c986761257
Jellive <mystral7@naver.com>
2020-03-01 00:20:47 +0900

    add module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import Checkbox from './components/CheckBox'
 
 function App() {
+  const [check, setCheck] = useState(false)
+  const onChange = (e: any) => {
+    setCheck(e.target.checked)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Checkbox onChange={onChange} checked={check}>
+        다음 약관에 모두 동의
+      </Checkbox>
+      <p>
+        <b>check: </b>
+        {check ? 'true' : 'false'}
+      </p>
     </div>
   );
 }
